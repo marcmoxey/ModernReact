904bc92e9436246d27fc6fb6dc467350a1780bf5
Marc Moxey <moxeymarc@gmail.com>
2025-11-11 12:34:50 -0500

    Created a list

--- ModernReact/Recact Fundamentals/Vite Dev Server and Buiid Tool/rating-ui/src/Components/Rating.jsx
+++ ModernReact/Recact Fundamentals/Vite Dev Server and Buiid Tool/rating-ui/src/Components/Rating.jsx
@@ -1,7 +1,13 @@
 const Rating = () => {
+    const stars = Array.from({length: 5}, (_,i) => + 1)
   return (
     <div className="rating-container">
       <h2>Rate Your Experience</h2>
+      <div className="stars">
+        {stars.map((star) => (
+            <span key={star} className="star">{'\u2605'}</span> 
+        ))}
+      </div>
     </div>
   );
 };
