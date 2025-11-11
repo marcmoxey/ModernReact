4761f47c4c9ce53184a3fd83a7d847e302e38720
Marc Moxey <moxeymarc@gmail.com>
2025-11-11 12:59:26 -0500

    learned event handlers

--- ModernReact/Recact Fundamentals/Vite Dev Server and Buiid Tool/rating-ui/src/Components/Rating.jsx
+++ ModernReact/Recact Fundamentals/Vite Dev Server and Buiid Tool/rating-ui/src/Components/Rating.jsx
@@ -1,11 +1,18 @@
 const Rating = () => {
-    const stars = Array.from({length: 5}, (_,i) => + 1)
+    const stars = Array.from({length: 5}, (_,i) => i + 1)
+    const clicked = (index) => console.log('Clicked', index);
+    const hoverd = (direction, index) => console.log('Hovered', direction, index);
+    
   return (
     <div className="rating-container">
       <h2>Rate Your Experience</h2>
       <div className="stars">
-        {stars.map((star) => (
-            <span key={star} className="star">{'\u2605'}</span> 
+        {stars.map((star, index) => (
+            <span onClick={ () => clicked(index)}   
+            onMouseEnter={ () => hoverd('enter', index)}
+            onMouseLeave={ () => hoverd('leave', index)}
+            key={star} 
+            className="star">{'\u2605'}</span> 
         ))}
       </div>
     </div>
