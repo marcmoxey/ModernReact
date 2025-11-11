49174e3d77f7e9eea1d154518f620ca4d4f6d706
Marc Moxey <moxeymarc@gmail.com>
2025-11-11 12:23:34 -0500

    Learn styles

--- ModernReact/Recact Fundamentals/Vite Dev Server and Buiid Tool/rating-ui/src/Components/Rating.jsx
+++ ModernReact/Recact Fundamentals/Vite Dev Server and Buiid Tool/rating-ui/src/Components/Rating.jsx
@@ -1,8 +1,18 @@
 const Rating = () => {
-  return ( 
-    <div>Rate Your Experience</div>
-  ) ;
+  return (
+    <div className="rating-container">
+      <h2>Rate Your Experience</h2>
+    </div>
+  );
 };
 
+// const styles = {
+//   container: {
+//     textAlign: "center",
+//     fontFamily: "Arial, sans-serif",
+//     padding: "20px",
+//   },
+//   heading: { color: 'red'}
+// }
 
 export default Rating;
