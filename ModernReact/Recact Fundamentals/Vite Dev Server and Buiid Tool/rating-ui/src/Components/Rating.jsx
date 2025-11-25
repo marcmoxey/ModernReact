a646dfb5601fbb6630d8a975b023429b635a16b2
Marc Moxey <moxeymarc@gmail.com>
2025-11-25 11:06:35 -0500

    Learn useState

--- ModernReact/Recact Fundamentals/Vite Dev Server and Buiid Tool/rating-ui/src/Components/Rating.jsx
+++ ModernReact/Recact Fundamentals/Vite Dev Server and Buiid Tool/rating-ui/src/Components/Rating.jsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
+
 const Rating = () => {
+  const [rating, setRating] = useState(0);
+  const[hover, setHover] = useState(0);
+
+
     const stars = Array.from({length: 5}, (_,i) => i + 1)
-    const clicked = (index) => console.log('Clicked', index);
-    const hoverd = (direction, index) => console.log('Hovered', direction, index);
+  
     
   return (
     <div className="rating-container">
       <h2>Rate Your Experience</h2>
       <div className="stars">
-        {stars.map((star, index) => (
-            <span onClick={ () => clicked(index)}   
-            onMouseEnter={ () => hoverd('enter', index)}
-            onMouseLeave={ () => hoverd('leave', index)}
+        {stars.map((star) => (
+            <span onClick={() => setRating(star)}
+            onMouseEnter={() => setHover(star)}
+            onMouseLeave={() => setHover(0) }
             key={star} 
             className="star">{'\u2605'}</span> 
         ))}
